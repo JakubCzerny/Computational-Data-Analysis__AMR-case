228950038e93dfa34c65578aa1d75c910244ec7f
JakubCzerny <kuba.czerny@poczta.fm>
2017-04-25 22:32:39 +0200

    Normalized and absolute number of resistant bacterias per samples per countries

--- app/utils/load_data.py
+++ app/utils/load_data.py
@@ -1,32 +1,42 @@
+from __future__ import division
 import csv
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 gene_counts_df = pd.read_csv('../../data/res_finder_gene_count.csv', delimiter='\t')
 gene_counts = gene_counts_df.ix[:, gene_counts_df.columns[1:]]
 
 cummulative = gene_counts.sum(axis=0)
 metadata = pd.read_csv('../../data/metadata.csv', delimiter='\t')
-country_sample = [(metadata.ix[i]['country'], cummulative[i]) for i in metadata.index]
+metadata = pd.read_csv('../../data/metadata.csv', delimiter='\t')
 
-cummulative_per_country = {}
-for x, y in country_sample:
-    cummulative_per_country.setdefault(x, []).append(y)
+country_sample = {}
+country_sample['absolute'] = [(metadata.ix[i]['country'], cummulative.loc[str(metadata.ix[i]['sample_code'])] ) for i in metadata.index]
+country_sample['normalized'] = [(metadata.ix[i]['country'], cummulative.loc[str(metadata.ix[i]['sample_code'])] / metadata.ix[i]['norm_Bacteria_pairs'] ) for i in metadata.index]
+
+options = ['normalized','absolute']
+cummulative_per_country = dict((option,{}) for option in options)
+
+for option in options:
+    for x, y in country_sample[option]:
+        cummulative_per_country[option].setdefault(x, []).append(y)
 
 
-fig = plt.figure()
-plot = fig.add_subplot(111)
-colours = ['black', 'red', 'blue', 'green', 'purple', 'aqua','black', 'red', 'blue', 'green', 'purple', 'aqua']
-width = 0.75
-ind = 0
-gap = 5
+# ======================================= PLOTING =======================================
+f, ax = plt.subplots(2, sharex=True)
 
-for i, (country, samples) in enumerate(cummulative_per_country.iteritems()):
-    print samples, country
-    colour = colours[i]
+for subplot, option in enumerate(options):
+    width = 0.75
+    ind = 0
+    gap = 5
 
-    for sample in samples:
-        rect = plot.bar(ind+i*gap, sample, width, color=colour)
-        ind += 1
+    for i, (country, samples) in enumerate(cummulative_per_country[option].iteritems()):
+        for sample in samples:
+            ax[subplot].bar(ind+i*gap, sample, width, color=cm.Vega20(i+1))
+            ind += 1
 
+ax[0].set_title('Normalized amount of resistant bacterias per sample per country')
+ax[1].set_title('Absolute number of resistant bacterias per sample per country')
 plt.show()
+# ========================================================================================
