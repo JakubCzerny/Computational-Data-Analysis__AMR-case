ae31ff94c3a7fab664c2831637d994c8a11da6c9
JakubCzerny <kuba.czerny@poczta.fm>
2017-04-26 03:00:17 +0200

    First attempt to build regression model

--- app/analysis.py
+++ app/analysis.py
@@ -1,10 +1,22 @@
+import numpy as np
 import matplotlib.pyplot as plt
-from utils.Data import Data
 import utils.plots as plots
+from utils.Data import Data
+from sklearn.linear_model import ElasticNet, ElasticNetCV, MultiTaskElasticNetCV
+from sklearn.model_selection import train_test_split
 
+#TODO We should look at metadata and discard wrong samples
 
-# We should look at metadata and discard wrong samples
+#IDEA:
+'''
 
+- extract the baterias that are present in most of the samples and try to do analysis on them
+- second option would be to select bacterias which amount varies a lot between the samples
+- high in absolute value (negative or possitive) correlation between presence of drug and presence of the resistent bacteria
+- cluster the samples based on the presence of bacterias (and their amount), we could use number of cluster equal to the #num_drugs*2 or *3 (should be still readable) => use colors for countries
+- linear regression (input drugs and their doses => amount of each bacteria)
+
+'''
 
 # Load the data files
 data = Data()
@@ -29,4 +41,39 @@ plots.binned_cummulative(cummulative_per_country, bins=2)
 plots.binned_cummulative(cummulative_per_country, bins=4)
 
 # Display all the plots together
-plt.show()
+# plt.show()
+
+
+
+'''
+Build a model for predicting the amount of each bacteria based on the used drags
+Apply sparse linear regression (ElasticNet) for easier analysis
+Force the coefficients to be non-negative as none drug should increase the presence of the bacterias
+'''
+
+# Use cut_off to get rid off samples that have less than cut_off amount of bacterias
+X,Y = data.get_gene_count_with_drugs(cut_off=30)
+print X.shape
+print Y.shape
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=1)
+
+folds = 5
+alphas = np.logspace(-8,3,10)
+l1_ratios = np.linspace(0,1,10,endpoint=True)
+
+# Elastic-net
+
+# NO IDEA WHY IT TELLS ME TO USE MultiTaskElasticNetCV
+
+# models = ElasticNetCV(l1_ratio=l1_ratios, alphas=alphas, verbose=1, cv=folds, positive=True, n_jobs=-1)
+models = MultiTaskElasticNetCV(l1_ratio=l1_ratios, alphas=alphas, verbose=1, cv=folds, n_jobs=-1)
+
+models.fit(X_train, Y_train)
+model.score(X_test, Y_test)
+
+model_EN = ElasticNet(l1_ratio=models.l1_ratio_, alpha=models.alpha_)
+model_EN.fit([X_train,X_test], [Y_train,Y_test])
+
+print "Alpha: ", models.alpha_
+print "L1 ratio: ", models.l1_ratio_
+print "Score of Elastic-net on test data: ", model.score(X_test, Y_test)
--- app/utils/Data.py
+++ app/utils/Data.py
@@ -8,6 +8,7 @@ class Data(object):
     def __init__(self):
         self.load()
 
+
     def load(self):
         self.gene_counts_df = pd.read_csv('../data/res_finder_gene_count.csv', delimiter='\t')
         self.metadata = pd.read_csv('../data/metadata.csv', delimiter='\t')
@@ -15,6 +16,7 @@ class Data(object):
         self.bacteria_count_df = pd.read_csv('../data/bacteria_genera_count.csv', delimiter='\t')
         self.country_drug_use_df = pd.read_csv('../data/country_drug_use.csv', delimiter='\t')
         self.flagged_samples_df = pd.read_csv('../data/flagged_samples.csv', delimiter='\t')
+
 
     def get_cummulatives(self):
         gene_counts = self.gene_counts_df.ix[:, self.gene_counts_df.columns[1:]]
@@ -32,3 +34,18 @@ class Data(object):
                 cummulative_per_country[option].setdefault(x, []).append(y)
 
         return cummulative_per_country
+
+
+    def get_gene_count_with_drugs(self, cut_off=None):
+        columns = [col for col in self.country_drug_use_df if 'ldu' in col]
+        drugs_per_country = dict((self.country_drug_use_df['country_2letter'].ix[i], self.country_drug_use_df[columns].ix[i]) for i in range(9))
+        sample_country = dict((self.metadata.ix[i]['sample_code'], self.metadata.ix[i]['country']) for i in range(self.metadata.shape[0]))
+
+        Y = np.array([drugs_per_country[sample_country[int(code)]].values for code in self.gene_counts_df.columns[1:]])
+        X = self.gene_counts_df.ix[:, self.gene_counts_df.columns[1:]].transpose().as_matrix()
+
+        if cut_off:
+            indices = np.where(X.sum(axis=0) < cut_off)[0]
+            X = np.delete(X, indices, axis=0)
+            Y = np.delete(Y, indices, axis=0)
+        return X,Y
