e17d4d66d8528d2991ba3b32402fe0f4045e5118
dokutagero <s16211@student.dtu.dk>
2017-05-02 12:22:14 +0200

    clustering with kmeans and silhouette figures obtained for all 3 datasets

--- app/clustering.py
+++ app/clustering.py
@@ -5,6 +5,8 @@ from collections import Counter
 from sklearn.metrics import silhouette_score, silhouette_samples
 from sklearn.preprocessing import MaxAbsScaler
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 
 
@@ -34,9 +36,13 @@ n_clusters = range(3,13)
 final_results = []
 cluster_info, clustered_individuals = {},{}
 
-for individuals in [individuals_df, pigs_df, poultry_df]:
+for individuals, category in zip([individuals_df, pigs_df, poultry_df], ['total individuals', 'pigs', 'poultry']):
 
     for n_cluster in n_clusters:
+
+        fig, ax1 = plt.subplots(1)
+        ax1.set_xlim([-0.2, 1])
+        ax1.set_ylim([0, len(individuals) + (len(n_clusters)+1)*10])
 
         kmeans = KMeans(n_clusters=n_cluster, n_jobs=3, n_init=50)
         # train_individuals, test_individuals = train_test_split(individuals.iloc[1:,:], train_size = 0.8)
@@ -49,6 +55,24 @@ for individuals in [individuals_df, pigs_df, poultry_df]:
 
         silhouette_average = silhouette_score(individuals.iloc[1:,:].values, cluster_labels)
         silhouette_values = silhouette_samples(individuals.iloc[1:,:].values, cluster_labels)
+        print 'Average score for ' + category +  ' with n_clusters ' + str(n_cluster) + ': ', silhouette_average
+
+        # Prepare plots
+        y_lower = 10
+        for i in range(len(n_clusters)):
+            ith_cluster_silhouette_values = silhouette_values[cluster_labels == i]
+            ith_cluster_silhouette_values.sort()
+            size_cluster_i = ith_cluster_silhouette_values.shape[0]
+            y_upper = y_lower + size_cluster_i
+
+            color = cm.spectral(float(i) / len(n_clusters))
+            ax1.fill_betweenx(np.arange(y_lower, y_upper), 0, ith_cluster_silhouette_values,
+                                facecolor=color, edgecolor=color, alpha=0.7)
+
+            # Plot labels
+            ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+            y_lower = y_upper + 10
+
 
         #let's determine which individuals belong to each cluster
         #for further exploration
@@ -83,6 +107,25 @@ for individuals in [individuals_df, pigs_df, poultry_df]:
             c = Counter(country_list)
             cluster_info[cluster]['predominant_country'] = c.most_common(2)
             cluster_info[cluster]['num_members'] = len(cluster_members)
-        print n_cluster
         final_results.append((individuals, clustered_individuals, cluster_info, silhouette_average, silhouette_values, kmeans))
         clustered_individuals, cluster_info = {}, {}
+
+        # Plotting
+        ax1.set_title("Silhouette plot for clusters")
+        ax1.set_xlabel("Silhouette coefficient values")
+        ax1.set_ylabel("Cluster label")
+
+        ax1.axvline(x=silhouette_average, color="red", linestyle='--')
+
+        ax1.set_yticks([])
+        ax1.set_xticks(np.arange(-0.5, 1, 0.1))
+        plt.suptitle(("Silhouette analysis for KMeans on " + category + " with num_clusters = ", n_cluster),
+                        fontsize=12, fontweight='bold')
+
+
+        # 2nd plot showing actual clusters formed
+        # Can't make it unless is 2D PCAed
+        # colors = cm.spectral(cluster_labels.astype(float) / len(n_clusters))
+        # ax2.scatter(individuals[:,0], X[:,1])
+
+plt.show()
